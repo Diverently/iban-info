5bb39ba65898ac2cf2735c88284be0a32aec3336
Robert Cordes <robert@diverently.com>
2022-09-28 15:41:35 +0200

    Test stubs

---
+++ tests/IbanDataTest.php
@@ -0,0 +1,9 @@
+<?php
+
+it('throws an error if the country code is not recognized', function () {
+    // expect(true)->toBeTrue();
+});
+
+it('gets the data for DE', function () {
+    // expect(true)->toBeTrue();
+})->skip();
---
+++ tests/IbanInfoTest.php
@@ -0,0 +1,9 @@
+<?php
+
+it('throws an error if the format is not recognized', function () {
+    // expect(true)->toBeTrue();
+})->skip();
+
+it('throws an error if the format is not recognized', function () {
+    expect(true)->toBeTrue();
+});
